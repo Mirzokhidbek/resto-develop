2364823a04db73157b5c79f8fa2487e3c9bd1a5f
Mirzokhidbek <mirodev@sju.ac.kr>
2026-09-02 08:29:44 +0900

    feat: implement routing

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import path from "path"
+import router from "./router"
 
 const app=express()
 console.log("__dirname:",__dirname)
@@ -12,4 +13,8 @@ app.use(express.json())
 
 app.set('view', path.join(__dirname,"views"));
 app.set('view engine', "ejs")
+
+
+
+app.use('/', router)
 export default app;
---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express"
+const router=express.Router()
+
+
+router.get('/',(req: Request, res:Response)=>{
+    res.send("You are on Home Page")
+ }) 
+
+ router.get('/login',(req: Request, res:Response)=>{
+    res.send("Login Page")
+ }) 
+
+ router.get('/signup',(req: Request, res:Response)=>{
+    res.send("Signup Page")
+ }) 
+
+ export default router;
