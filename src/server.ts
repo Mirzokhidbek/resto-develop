d1d6c9258a2e710b42baef812008d23cf163eb80
Mirzokhidbek <mirodev@sju.ac.kr>
2026-09-02 04:31:04 +0900

    Initial backend setup

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import moment from "moment";
+
+const port = 3000;
+
+console.log("Burak backend server started");
+console.log(`Current time: ${moment().format("YYYY-MM-DD HH:mm:ss")}`);
+console.log(`Server listening on port ${port}`);
